571a4e0a3c0469cf5ff1c0a6e996bebd1366bf07
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-09-14 11:24:33 -0400

    defangs user response

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import defang from 'defang';
 import axios from 'axios';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000
+
+// function for when user sends their typed response
+app.get('/compare', async(req, res) => {
+    const {userResponse} = req.query;
+
+    if (!userResponse)
+        return res.status(400).json({ error: "user response is missing!"});
+
+    // uses defang to sanitize input
+    const defangedResponse = defang(userResponse);
+})
