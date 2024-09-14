111509559454d427ced4c284f96f178adc3a8fbb
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2024-09-14 11:47:43 -0400

    first cohere grammar response category

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import defang from 'defang';
 import axios from 'axios';
+import cohere from 'cohere-ai';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+// prompt for telling cohere about the project
+const header_prompt =   "I'm making a game where the user is trying to impersonate an AI. Please judge all responses by seeing how similar they are to what an AI would say. \
+                        There are four categories of prompts - Grammar, Tone, Word Choice, Length - each scored out of 25 for a total score of 100. A response that's extremely similar to an AI should score 100, while one \
+                        that is too 'human' should receive 0 points. For the output, you should output the number out of 25 for that category, followed by a space, then exactly one short, concise sentence justifying the mark. \
+                        As an example, for Grammar: '18 Response contained a comma splice and two mispelled words'";
+
+cohere.init(process.env.COHERE_API_KEY);
 
 // function for when user sends their typed response
 app.get('/compare', async(req, res) => {
@@ -17,4 +26,26 @@ app.get('/compare', async(req, res) => {
 
     // uses defang to sanitize input
     const defangedResponse = defang(userResponse);
+
+    // call cohere's api to get scores for each category
+    try {
+        const cohereGrammar = await cohere.generate({
+            model: 'medium',
+            prompt: `${header_prompt} For this category, mark the grammar of this response out of 25. Start at 25 points, and with each significant grammar mistake, deduct some points. 
+                    Here is the user's response: ${defangedResponse}`,
+            max_tokens: 150,
+            temperature: 0.75, 
+        });
+
+        const cohereGrammarResponse = cohereGrammar.body.generations[0].text.trim();
+
+        // shift the index along while the character is still a digit
+        let numIndex = 0;
+        while ('0' <= cohereGrammarResponse[numIndex] && cohereGrammarResponse[numIndex] <= '9') numIndex++;
+
+        res.json({
+            score: cohereGrammarResponse
+        })
+    }
+
 })
